4f3f8298e812e8d15453ae1a93a42ff27082208b
Max Zhurov <greenbearrus@gmail.com>
2018-09-07 17:32:08 +0300

    Back button has been created. Added some texts to earthquake activity.

--- app/src/main/java/ru/mzhurov/seismonitor/ui/EarthquakeActivity.java
+++ app/src/main/java/ru/mzhurov/seismonitor/ui/EarthquakeActivity.java
@@ -3,8 +3,10 @@ package ru.mzhurov.seismonitor.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +18,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import ru.mzhurov.seismonitor.R;
 import ru.mzhurov.seismonitor.ui.model.Earthquake;
 
@@ -24,14 +31,20 @@ public class EarthquakeActivity extends AppCompatActivity implements OnMapReadyC
     private static final String MAP_BUNDLE      = "MAP_BUNDLE";
     private static final int    EARTHQUAKE_ITEM = R.string.earthquake_activity_bundle_key;
 
-    private MapView    mapView;
-    private GoogleMap  googleMap;
-    private Earthquake earthquake;
+    final Calendar         calendar         = Calendar.getInstance();
+    final TimeZone         timeZone         = calendar.getTimeZone();
+    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm dd-MM-yyyy");
+
+    private MapView   mapView;
+    private GoogleMap googleMap;
+    private Earthquake earthquake = new Earthquake();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
+
+        simpleDateFormat.setTimeZone(timeZone);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
 
@@ -42,75 +55,38 @@ public class EarthquakeActivity extends AppCompatActivity implements OnMapReadyC
             mapBundle = savedInstanceState.getBundle(MAP_BUNDLE);
         }
 
+        String description = null;
+
         if (intent != null) {
             final String key = getString(EARTHQUAKE_ITEM);
             earthquake = (Earthquake) intent.getSerializableExtra(key);
 
-            toolbar.setTitle(earthquake.getDescription());
+            description = earthquake.getDescription();
+
+            toolbar.setTitle(description);
         }
 
         setSupportActionBar(toolbar);
 
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
         mapView = findViewById(R.id.earthquake_map_view);
         mapView.onCreate(mapBundle);
         mapView.getMapAsync(this);
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+        final TextView earthquakeTitleTextView = findViewById(R.id.text_view_earthquake_title);
+        earthquakeTitleTextView.setText(description);
 
-        mapView.onResume();
-    }
+        final long time = earthquake.getTime();
+        final String timeString = simpleDateFormat.format(new Date(time));
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        mapView.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        mapView.onStop();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        mapView.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-
-        mapView.onLowMemory();
-    }
-
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        Bundle mapViewBundle = outState.getBundle(MAP_BUNDLE);
-
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle(MAP_BUNDLE, mapViewBundle);
-        }
-
-        mapView.onSaveInstanceState(mapViewBundle);
+        final TextView earthquakeIncidentTimeTextView = findViewById(R.id.text_view_earthquake_incident_time);
+        earthquakeIncidentTimeTextView.setText(timeString);
     }
 
     @Override
@@ -153,5 +129,68 @@ public class EarthquakeActivity extends AppCompatActivity implements OnMapReadyC
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+
+        return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_BUNDLE);
+
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_BUNDLE, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        mapView.onLowMemory();
     }
 }
